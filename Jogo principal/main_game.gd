8513ba96488bc0640053ac092361a8dd3e72e502
Fernando <fernando_macedo09@hotmail.com>
2026-05-07 15:23:20 -0300

    Botoes funcionando no menu

--- Jogo principal/main_game.gd
+++ Jogo principal/main_game.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,16 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_start_pressed() -> void:
-	print("Iniciar jogo apertado")
-
-
-func _on_creditos_pressed() -> void:
-	print("Creditos apertado")
-
-
-func _on_exit_pressed() -> void:
-	print("Sair apertado")
-	get_tree().quit()
